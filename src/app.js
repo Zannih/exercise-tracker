2cec0a2a81f71759c92ab7918f7127a5f2e0e508
James Raffan <82840677+Zannih@users.noreply.github.com>
2021-05-22 22:06:43 -0700

    reduced button functions to single call function w/ params

--- src/app.js
+++ src/app.js
@@ -1,100 +1,19 @@
 
-//goal and completion setters called by button click
-let pushupsGoal = 0;
-const incPushupsGoal = () => {
-    pushupsGoal += Number(document.getElementById('act-1-goal').value);
-    document.getElementById('pushups-goal').innerHTML = pushupsGoal;
-    getPushupsBool();
-}
-let pushupsDone = 0;
-const incPushupsComplete = () => {
-    pushupsDone += Number(document.getElementById('act-1-complete').value);
-    document.getElementById('pushups-complete').innerHTML = pushupsDone;
-    getPushupsBool();
-}
 
-let squatsGoal = 0;
-const incSquatsGoal = () => {
-    squatsGoal += Number(document.getElementById('act-2-goal').value);
-    document.getElementById('squats-goal').innerHTML = squatsGoal;
-    getSquatsBool();
-}
-let squatsDone = 0;
-const incSquatsComplete = () => {
-    squatsDone += Number(document.getElementById('act-2-complete').value);
-    document.getElementById('squats-complete').innerHTML = squatsDone;
-    getSquatsBool();
-}
+//parameter 'activity' and 'activity2' replace the need to have functions specific to each exercise
+//button click calls incValue() with specific id/names
+let valueToAdd = 0;
+const incValue = (activity, activityTwo) => {
+    valueToAdd += Number(document.getElementById(activity).value);
+    document.getElementById(activityTwo).innerHTML = valueToAdd + Number(document.getElementById(activityTwo).innerHTML);
+    valueToAdd = 0;
+} 
 
-let crunchesGoal = 0;
-const incCrunchesGoal = () => {
-    crunchesGoal += Number(document.getElementById('act-3-goal').value);
-    document.getElementById('crunches-goal').innerHTML = crunchesGoal;
-    getCrunchesBool();
-}
-let crunchesDone = 0;
-const incCrunchesComplete = () => {
-    crunchesDone += Number(document.getElementById('act-3-complete').value);
-    document.getElementById('crunches-complete').innerHTML = crunchesDone;
-    getCrunchesBool();
-}
-
-let planksGoal = 0;
-const incPlanksGoal = () => {
-    planksGoal += Number(document.getElementById('act-4-goal').value);
-    document.getElementById('planks-goal').innerHTML = planksGoal;
-    getPlanksBool();
-}
-let planksDone = 0;
-const incPlanksComplete = () => {
-    planksDone += Number(document.getElementById('act-4-complete').value);
-    document.getElementById('planks-complete').innerHTML = planksDone;
-    getPlanksBool();
-}
-
-let walkingGoal = 0;
-const incWalkingGoal = () => {
-    walkingGoal += Number(document.getElementById('act-5-goal').value);
-    document.getElementById('walking-goal').innerHTML = walkingGoal;
-    getWalkingBool();
-}
-let walkingDone = 0;
-const incWalkingComplete = () => {
-    walkingDone += Number(document.getElementById('act-5-complete').value);
-    document.getElementById('walking-complete').innerHTML = walkingDone;
-    getWalkingBool();
-}
-
-let meditationGoal = 0;
-const incMeditationGoal = () => {
-    meditationGoal += Number(document.getElementById('act-6-goal').value);
-    document.getElementById('meditation-goal').innerHTML = meditationGoal;
-    getMeditationBool();
-}
-let meditationDone = 0;
-const incMeditationComplete = () => {
-    meditationDone += Number(document.getElementById('act-6-complete').value);
-    document.getElementById('meditation-complete').innerHTML = meditationDone;
-    getMeditationBool();
-}
-
-let stretchingGoal = 0;
-const incStretchingGoal = () => {
-    stretchingGoal += Number(document.getElementById('act-7-goal').value);
-    document.getElementById('stretching-goal').innerHTML = stretchingGoal;
-    getStretchingBool();
-}
-let stretchingDone = 0;
-const incStretchingComplete = () => {
-    stretchingDone += Number(document.getElementById('act-7-complete').value);
-    document.getElementById('stretching-complete').innerHTML = stretchingDone;
-    getStretchingBool();
-}
 
 //bool functions
 let pushupsBool = false;
 const getPushupsBool = () => {
-    if (pushupsDone >= pushupsGoal && pushupsGoal > 0) {
+    if (Number(document.getElementById('pushupsComplete').innerHTML) >= Number(document.getElementById('pushupsGoal').innerHTML) && Number(document.getElementById('pushupsGoal').innerHTML) > 0) {
         pushupsBool = true;
     } else {
         pushupsBool = false;
@@ -103,7 +22,7 @@ const getPushupsBool = () => {
 
 let squatsBool = false;
 const getSquatsBool = () => {
-    if (squatsDone >= squatsGoal && squatsGoal > 0) {
+    if (Number(document.getElementById('squatsComplete').innerHTML) >= Number(document.getElementById('squatsGoal').innerHTML) && Number(document.getElementById('squatsGoal').innerHTML) > 0) {
         squatsBool = true;
     } else {
         squatsBool = false;
@@ -112,7 +31,7 @@ const getSquatsBool = () => {
 
 let crunchesBool = false;
 const getCrunchesBool = () => {
-    if (crunchesDone >= crunchesGoal && crunchesGoal > 0) {
+    if (Number(document.getElementById('crunchesComplete').innerHTML) >= Number(document.getElementById('crunchesGoal').innerHTML) && Number(document.getElementById('crunchesGoal').innerHTML) > 0) {
         crunchesBool = true;
     } else {
         crunchesBool = false;
@@ -121,7 +40,7 @@ const getCrunchesBool = () => {
 
 let planksBool = false;
 const getPlanksBool = () => {
-    if (planksDone >= planksGoal && planksGoal > 0) {
+    if (Number(document.getElementById('planksComplete').innerHTML) >= Number(document.getElementById('planksGoal').innerHTML) && Number(document.getElementById('planksGoal').innerHTML) > 0) {
         planksBool = true;
     } else {
         planksBool = false;
@@ -130,7 +49,7 @@ const getPlanksBool = () => {
 
 let walkingBool = false;
 const getWalkingBool = () => {
-    if (walkingDone >= walkingGoal && walkingGoal > 0) {
+    if (Number(document.getElementById('walkingComplete').innerHTML) >= Number(document.getElementById('walkingGoal').innerHTML) && Number(document.getElementById('walkingGoal').innerHTML) > 0) {
         walkingBool = true;
     } else {
         walkingBool = false;
@@ -139,7 +58,7 @@ const getWalkingBool = () => {
 
 let meditationBool = false;
 const getMeditationBool = () => {
-    if (meditationDone >= meditationGoal && meditationGoal > 0) {
+    if (Number(document.getElementById('meditationComplete').innerHTML) >= Number(document.getElementById('meditationGoal').innerHTML) && Number(document.getElementById('meditationGoal').innerHTML) > 0) {
         meditationBool = true;
     } else {
         meditationBool = false;
@@ -148,7 +67,7 @@ const getMeditationBool = () => {
 
 let stretchingBool = false;
 const getStretchingBool = () => {
-    if (stretchingDone >= stretchingGoal && stretchingGoal > 0) {
+    if (Number(document.getElementById('stretchingComplete').innerHTML) >= Number(document.getElementById('stretchingGoal').innerHTML) && Number(document.getElementById('stretchingGoal').innerHTML) > 0) {
         stretchingBool = true;
     } else {
         stretchingBool = false;
@@ -158,6 +77,13 @@ const getStretchingBool = () => {
 //call bool functions and tally them as an integer, called by 'get result button'
 let goalsMet = 0;
 const calcGoalsMet = () => {
+    getPushupsBool();
+    getSquatsBool();
+    getCrunchesBool();
+    getPlanksBool();
+    getWalkingBool();
+    getMeditationBool();
+    getStretchingBool();
     goalsMet = (
             (pushupsBool ? 1 : 0)
             + (squatsBool ? 1 : 0)
